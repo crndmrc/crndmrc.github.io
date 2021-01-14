16bd2c42abe3b656f6f36134fd2008ae5d2cc63e
crndmrc <56431511+crndmrc@users.noreply.github.com>
2021-01-14 17:39:38 +0300

    Add files via upload

---
+++ ozgecmis.php
@@ -0,0 +1,85 @@
+<?php
+session_start();
+?>
+<?php
+ include('css/css.php');
+?> 
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Hakkında</title>
+	<style type="text/css">
+		.contanier{
+			 width: 900px;
+    height: 10px;
+    padding: 52px;
+    position: absolute;
+    margin-left: 350px;
+
+		}
+		.thumbnail{
+			background-color: #d6d6d6;
+			 width: 700px;
+    height: 800px;
+    padding: 52px;
+    position: absolute;
+    top: 15%;
+    left: 25%;
+    margin-left: 25px;
+    margin-top: 50px;
+		}
+		.thumbnail12{
+			background-color: #d6d6d6;
+			 width: 150px;
+    height:150px;
+    padding: 52px;
+    position: absolute;
+    top: 15%;
+    left: 115%;
+    margin-left: 25px;
+    margin-top: 50px;
+		}
+		.ozgecmis{
+			position: absolute;
+			top: 40%;
+			  color: #23152d;
+           font-family:Comic Sans MS, Helvetica, sans-serif;
+            font-size:15px;
+		}
+		h1{
+			position: absolute;
+		left: 35%;
+		top: 10%;
+			color: #23152d;
+           font-family:Comic Sans MS, Helvetica, sans-serif;
+            font-size:35px;
+		}
+		.resim{
+			position: absolute;
+			top: 10%;
+			left: 70%;
+		}
+
+	</style>
+</head>
+<body>
+	<body background="resimler/1.jpg"> </body>
+
+	<div class="container" >
+
+             <div class="container" >
+ 
+      <div class="thumbnail" id="thumb1">
+     <img class="resim"src="resimler/ceren.jpg" width="150" height="150" alt="bu ceren"/><h1> <b>ÖZGEÇMİŞ</b></h1>
+ 
+        <div class="ozgecmis">Hayatım boyunca süre gelen araştırmacı ruhum çocukluğumda çevreme olan küçük meraklar ve teknolojiye olan ilgim ile başladı. Yeni keşifler ve bilgiler edinmek beni hep heyecenlandırıyordu. Gördüğü her şey hakkında “daha iyisi, daha kusursuzu nasıl olurdu?” gibi sorularla meşgul bir çocuktum ben. Kişisel gelişime meraklı ve kendini tanımaya açık birisiyim.  Tabi ki üniversite seçimlerim sırasında da kendimi tanıyarak bir seçim yapmam gerekiyordu ve hem analitik hem hayalperest bir zeka hangi meslek dalına daha uygundur düşünceleri sarmıştı kafamı. En sonunda hayallerimi süsleyebilecek olan bilgisayar mühendisliği bölümünü okumaya karar verdim.
+6 yaşında babasını kaybeden birisi olarak da hayatın zorluklarıyla küçük yaşta başbaşa kaldım. Lise ve üniversite hayatlarımı devlet yurtlarında yatılı olarak okudum. Yatılı okumam her şeyden önce büyük sorumluluklar yüklesede kişiliğimin en büyük temellerini oluşturdu. İnsanlarla iletişimi, maddi olarak geçinmeyi, zor anlarla başa çıkmayı ve kusurlarını görmekten korkmamayı, düzeltmek için elinden geleni yapmayı öğretti bana. 
+Bilgisayarın her alanı önemli olsa da programlama kısmı her daim ilgimi çekmiştir. Öyle ki ne zaman bilgisayar başına geçip programlama yapsam vaktin nasıl geçtiğini anlamamdan belli olmaktadır.. Bilgisayar Mühendisliği alanında staj yapma amacım insanlığa ve vatanıma faydalı olacak üretimlerde yer almak ve doğrudan katkı sağlamayı istemek olarak bahsedebilirim. Bölümümde kurumunuz gibi üst düzey bir yerde staj yaparak kariyerimi başlatmak ve kendimi eğitmek isterim. </div>
+      </a>
+<a  href="fotograflar.php"><figcaption>Fotoğraf albümüne bakmak için tıklayınız</figcaption></a>
+      </div>
+</div></div>
+
+		
+</body>
+</html>
